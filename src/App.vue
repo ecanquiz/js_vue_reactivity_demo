d629f338de0a1681aee65e98621804d4613cfbf3
Ernesto Canquiz <fdsoil123@gmail.com>
2020-08-26 13:20:50 -0400

    Reactivity Demo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    h1 Reactividad
+    p {{ person }}
+    button(@click='addProp') Add Prop
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return { 
+      person: {
+        name: 'Ignacio'
+      }
+    }
+  },
+  methods: {
+    addProp () {
+      //this.person.lastName = 'Anaya'
+      this.$set(this.person, 'lastName', 'Anaya')
+      this.person = Object.assign({}, this.person, { a:1, b:2 })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  
+</style>
